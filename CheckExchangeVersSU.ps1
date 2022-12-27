779365062eaac67e3535ebb0a1fc315fa923218c
mzkk000 <90523947+mzkk000@users.noreply.github.com>
2022-12-27 19:48:18 +0100

    Update and rename CheckExchangeVersSU to CheckExchangeVersSU.ps1

--- CheckExchangeVersSU.ps1
+++ CheckExchangeVersSU.ps1
@@ -1,3 +1,4 @@
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 $ExchangeServers = Get-ExchangeServer | Sort-Object Name
 ForEach ($Server in $ExchangeServers) {
     Invoke-Command -ComputerName $Server.Name -ScriptBlock { Get-Command Exsetup.exe | ForEach-Object { $_.FileversionInfo } }
